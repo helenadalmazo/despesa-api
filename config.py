11dbfd4925431f87098ce6625eb0be3e8098ccfe
Helena Dalmazo <helenadalmazo@outlook.com>
2020-06-20 16:48:53 -0300

    Adding SQLAlchemy to project and configuring in the flask app

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+database_filename = "despesa_api.db"
 
 class Config:
     DEBUG = True
     SECRET_KEY = "dXNlcm5hbWU6cGFzc3dvcmQ="
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + database_filename
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,15 +1,23 @@
 from flask import Flask
+import os.path
 
-from config import Config
+from config import Config, database_filename
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object(Config)
 
+    from database.model import database
+    database.init_app(app)
+
     from blueprint.app import app_blueprint
 
     app.register_blueprint(app_blueprint)
+
+    with app.app_context():
+        if os.path.exists(database_filename) == False:
+            database.create_all() 
 
     return app
 
