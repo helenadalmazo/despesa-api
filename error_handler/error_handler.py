a16aa96d965a4b000447226f353b1a2957ba3357
Helena Dalmazo <helenadalmazo@outlook.com>
2021-04-10 17:13:51 -0300

    Adding handler for HTTPException

--- error_handler/error_handler.py
+++ error_handler/error_handler.py
@@ -17,6 +17,10 @@ def handle_validation_exception(exception):
     return jsonify({"message": exception.message, "errors": exception.errors}), exception.status_code
 
 
+def handle_http_exception(exception):
+    return jsonify({"message": exception.description}), exception.code
+
+
 def handle_exception(exception):
     print(exception)
     return jsonify({"message": "Ocorreu um erro interno no servidor"}), 500
