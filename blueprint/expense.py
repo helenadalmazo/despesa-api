a09e12935c2071f0b0964a0db9dda3b41b3c92e2
Helena Dalmazo <helenadalmazo@outlook.com>
2021-04-16 21:54:43 -0300

    Balance of expenses

--- blueprint/expense.py
+++ blueprint/expense.py
@@ -1,3 +1,4 @@
+import copy
 from datetime import datetime
 
 from flask import Blueprint, jsonify, request
@@ -23,14 +24,14 @@ user_repository = UserRepository()
 
 @expense_blueprint.route("/group/<int:group_id>", methods=["GET"])
 @token_required
-def list(current_user, group_id):
+def index(current_user, group_id):
     group = group_repository.get_or_404(current_user, group_id)
 
     today = datetime.now()
     month = request.args.get("month", today.month)
     year = request.args.get("year", today.year)
 
-    expense_list = expense_repository.list_by_month_year(group, month, year)
+    expense_list = expense_repository.list_by_year_month(group, year, month)
 
     return jsonify([expense.json() for expense in expense_list])
 
@@ -126,6 +127,66 @@ def list_categories(current_user):
     return jsonify([expense_category.json() for expense_category in expense_category_list])
 
 
+@expense_blueprint.route("/group/<int:group_id>/balance", methods=["GET"])
+@token_required
+def balance(current_user, group_id):
+    group = group_repository.get_or_404(current_user, group_id)
+
+    today = datetime.now()
+    month = request.args.get("month", today.month)
+    year = request.args.get("year", today.year)
+
+    expense_list = expense_repository.list_by_year_month(group, year, month)
+
+    balance_list = []
+    current_user_statement_list = []
+
+    for expense in expense_list:
+        credit_balance_item = get_balance_item(balance_list, expense.created_by)
+        credit_balance_item["value"] = credit_balance_item["value"] + expense.value
+
+        if expense.created_by == current_user.id:
+            credit_statement_item = {
+                "expense_id": expense.id,
+                "name": expense.name,
+                "value": expense.value
+            }
+            current_user_statement_list.append(credit_statement_item)
+
+        for item in expense.items:
+            debit_balance_item = get_balance_item(balance_list, item.user_id)
+            debit_balance_item["value"] = debit_balance_item["value"] + item.value * -1
+
+            if item.user_id == current_user.id:
+                debit_statement_item = {
+                    "expense_id": expense.id,
+                    "expense_item_id": item.id,
+                    "name": expense.name,
+                    "value": item.value * -1
+                }
+                current_user_statement_list.append(debit_statement_item)
+
+    balance = sum(balance_item["value"] for balance_item in balance_list)
+    if balance != 0:
+        raise Exception(f"O balancete do grupo [{group_id}] não fechou.")
+
+    split_list = get_split_list(copy.deepcopy(balance_list))
+
+    current_user_split_list = []
+    for split_item in split_list:
+        if split_item["receiver"] == current_user.id or split_item["payer"] == current_user.id:
+            current_user_split_item = {
+                "payer": user_repository.get(split_item["payer"]).json(),
+                "receiver": user_repository.get(split_item["receiver"]).json(),
+                "value": split_item["value"]
+            }
+            current_user_split_list.append(current_user_split_item)
+
+    current_user_balance = next(filter(lambda b: b["user_id"] == current_user.id, balance_list), None)["value"]
+
+    return jsonify({"statement": current_user_statement_list, "balance": current_user_balance, "split": current_user_split_list})
+
+
 def save_items(expense, data_items, group):
     value = None
     users = []
@@ -206,3 +267,61 @@ def notify(trigger_user, group, expense):
             )
 
             response = messaging.send(message)
+
+
+def get_balance_item(balance_list, user_id):
+    balance_item = next(filter(lambda bi: bi["user_id"] == user_id, balance_list), None)
+    if balance_item:
+        return balance_item
+
+    balance_item = {
+        "user_id": user_id,
+        "statements": [],
+        "value": 0
+    }
+    balance_list.append(balance_item)
+    return balance_item
+
+
+def get_split_list(balance_list):
+    credit_balance_list = list(filter(lambda si: si["value"] > 0, balance_list))
+    credit_balance_list = sorted(credit_balance_list, key=lambda si: si["value"], reverse=True)
+
+    debit_balance_list = list(filter(lambda si: si["value"] < 0, balance_list))
+
+    def lambda_positive_value(si):
+        si["value"] = si["value"] * -1
+        return si
+
+    debit_balance_list = list(map(lambda si: lambda_positive_value(si), debit_balance_list))
+    debit_balance_list = sorted(debit_balance_list, key=lambda si: si["value"], reverse=True)
+
+    split_list = []
+
+    for credit_balance_item in credit_balance_list:
+        while credit_balance_item["value"] > 0:
+            split_item = {
+                "receiver": credit_balance_item["user_id"]
+            }
+
+            debit_balance_item = debit_balance_list[0]
+
+            split_item["payer"] = debit_balance_item["user_id"]
+
+            value = 0
+
+            if debit_balance_item["value"] > credit_balance_item["value"]:
+                value = credit_balance_item["value"]
+                debit_balance_item["value"] = debit_balance_item["value"] - value
+            else:
+                value = debit_balance_item["value"]
+                debit_balance_list.remove(debit_balance_item)
+
+            credit_balance_item["value"] = credit_balance_item["value"] - value
+
+            split_item["value"] = value
+            split_list.append(split_item)
+
+        credit_balance_list.remove(credit_balance_item)
+
+    return split_list
